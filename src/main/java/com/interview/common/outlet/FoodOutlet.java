3d853aa160f3c440a69abd97f285fbd1549155c0
NV <NVardell@GMail.com>
2024-01-09 20:16:50 -0500

    food outlet model

---
+++ src/main/java/com/interview/common/outlet/FoodOutlet.java
@@ -0,0 +1,18 @@
+package com.interview.common.outlet;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Individual Food Outlet Model
+ *
+ * @author NV
+ * @since 1/9/2024
+ */
+@Data @Builder public class FoodOutlet {
+    private int id;
+    private String name;
+    private String co;
+    private int estimated_cost;
+    private UserRating user_rating;
+}
