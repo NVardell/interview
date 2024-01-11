62482fa208824c2393d4ab0ca4e3da051442000d
NV <NVardell@GMail.com>
2024-01-10 20:20:58 -0500

    no real change

--- src/test/java/com/interview/_2023/spokeo/Spokeo_1.java
+++ src/test/java/com/interview/_2023/spokeo/Spokeo_1.java
@@ -2,6 +2,9 @@ package com.interview._2023.spokeo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * NAME
  *      -
@@ -13,15 +16,17 @@ import org.junit.jupiter.api.Test;
  *      In ~
  *      Out ~
  *      Note ~
- * import static org.hamcrest.MatcherAssert.assertThat;
- * import static org.hamcrest.Matchers.is;
  *
  * @author NV
  * @since 1/3/2024
  */
 public class Spokeo_1 {
 
-    @Test void testStuff() {
+    private boolean solution() {
+        return true;
+    }
 
+    @Test void testSolution() {
+        assertThat(solution(), is(true));
     }
 }
