6f5fe0e000348f906df564b4527a2229e722a77f
NV <NVardell@GMail.com>
2024-01-10 20:22:00 -0500

    templates

---
+++ src/test/java/com/interview/_2023/attentive/Attentive_1.java
@@ -0,0 +1,32 @@
+package com.interview._2023.attentive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * NAME
+ *      -
+ * NOTES
+ *      -
+ * CONSTRAINTS
+ *      -
+ * EXAMPLE #1
+ *      In ~
+ *      Out ~
+ *      Note ~
+ *
+ * @author NV
+ * @since 1/10/2024
+ */
+public class Attentive_1 {
+
+    private boolean solution() {
+        return true;
+    }
+
+    @Test void testSolution() {
+        assertThat(solution(), is(true));
+    }
+}
