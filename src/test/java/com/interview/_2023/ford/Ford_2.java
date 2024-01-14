6cc2ecc7971220c69e4b47681604202217231930
NV <NVardell@GMail.com>
2024-01-14 18:27:57 -0500

    added template for q2

---
+++ src/test/java/com/interview/_2023/ford/Ford_2.java
@@ -0,0 +1,32 @@
+package com.interview._2023.ford;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * NAME
+ *      -
+ * NOTES
+ *      -
+ * CONSTRAINTS
+ *      -
+ * EXAMPLE #1
+ *      In ~
+ *      Out ~
+ *      Note ~
+ *
+ * @author NV
+ * @since 1/3/2024
+ */
+public class Ford_2 {
+
+    private boolean solution() {
+        return true;
+    }
+
+    @Test void testSolution_1() {
+        assertThat(solution(), is(true));
+    }
+}
