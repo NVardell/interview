59a073475e46c5ef2d0f1e256869add04a9abdf1
NV <NVardell@GMail.com>
2024-01-14 18:27:30 -0500

    added test & method stubs

--- src/test/java/com/interview/_2023/template/Template.java
+++ src/test/java/com/interview/_2023/template/Template.java
@@ -1,5 +1,10 @@
 package com.interview._2023.template;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * NAME
  *      -
@@ -11,11 +16,17 @@ package com.interview._2023.template;
  *      In ~
  *      Out ~
  *      Note ~
- * import static org.hamcrest.MatcherAssert.assertThat;
- * import static org.hamcrest.Matchers.is;
  *
  * @author NV
  * @since 1/11/2022
  */
 public class Template {
+
+    private boolean solution() {
+        return true;
+    }
+
+    @Test void testSolution_1() {
+        assertThat(solution(), is(true));
+    }
 }
