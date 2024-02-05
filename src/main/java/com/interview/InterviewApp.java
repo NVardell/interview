51a9253679763ee6b55a3b0d2f35b19d9518dc2b
NV <NVardell@GMail.com>
2024-02-04 21:11:55 -0500

    name refactor

--- src/main/java/com/interview/InterviewApp.java
+++ src/main/java/com/interview/InterviewApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterviewsApplication {
+public class InterviewApp {
 
 	public static void main(String... args) {
-		SpringApplication.run(InterviewsApplication.class, args);
+		SpringApplication.run(InterviewApp.class, args);
 	}
 
 }
