6b75d14f853b39fd9e6a93df022954a408b070c2
NV <NVardell@GMail.com>
2024-02-05 18:38:22 -0500

    solution cleanup

--- src/test/java/com/interview/_2023/apple/Apple_1.java
+++ src/test/java/com/interview/_2023/apple/Apple_1.java
@@ -27,33 +27,20 @@ public class Apple_1 {
     private int solution(String s) {
 
         StringBuilder sb = new StringBuilder(s);
-        boolean looping = true, match = false;
+        boolean looping = true, match;
 
         while(looping) {
             match=false;
-            for (int i = 0; i < sb.length() - 1; i++) {
-                System.out.println("i=" + i);
-                if (sb.charAt(i) == '0' && sb.charAt(i + 1) == '1' || sb.charAt(i) == '1' && sb.charAt(i + 1) == '0') {
-                    System.out.println("length=" + sb.length() + "\tsb=" + sb);
+            for (int i = 0; i < sb.length() - 1; i++)
+                if (sb.charAt(i) == '0' && sb.charAt(i + 1) == '1'
+                        || sb.charAt(i) == '1' && sb.charAt(i + 1) == '0') {
                     sb.delete(i, i + 2);
-                    System.out.println("length=" + sb.length() + "\tsb=" + sb);
-                    match=true;
-                    System.out.println("breaking from if");
+                    match = true;
                     break;
                 }
-//                else {
-//                    System.out.println("ELSE\tELSE\tELSE\tELSE\t");
-//                    looping = false;
-//                }
-                System.out.println("end of for");
-
-//                else looping=false;
-            }
-
 
             if(sb.length()==1 || !match)
                 looping=false;
-            System.out.println("end of while \t looping = " + looping);
         }
 
         return sb.length();
