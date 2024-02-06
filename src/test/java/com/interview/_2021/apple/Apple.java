465ac50f523747e5a3c4e29a11692794306f27b2
NV <NVardell@GMail.com>
2024-02-05 21:37:42 -0500

    added 2021 apple assessment

---
+++ src/test/java/com/interview/_2021/apple/Apple.java
@@ -0,0 +1,16 @@
+package com.interview._2021.apple;
+
+/**
+ * NAME -
+ * COMPANY -
+ * NOTES -
+ * CONSTRAINTS -
+ * EXAMPLE #1     In ~     Out ~     Note ~
+ * import static org.hamcrest.MatcherAssert.assertThat;
+ * import static org.hamcrest.Matchers.is;
+ *
+ * @author NV
+ * @since 1/11/2022
+ */
+public class Apple {
+}
